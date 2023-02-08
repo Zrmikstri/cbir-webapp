8c650c3d6df5a6423f308da2d3b7824ca19ee1d7
Zrmikstri <tpvinhhcm@gmail.com>
2023-02-08 22:40:04 +0700

    complete basic functionality of the app

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources=r'/api/*')
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+@app.route('/api/test2', methods=['POST'])
+def test2():
+    from utils import base64URL_to_image, image_to_base64URL 
+    
+    # Decode query image sent by user
+    image = base64URL_to_image(request.form.get('image'))
+    value = request.form.get('value')
+    
+    # Extract fetures from query image
+
+    # Search in database
+
+    # Return results
+    response_data = [{'image': image_to_base64URL(image), 'value': value} for i in range(50)]
+
+    response = make_response(jsonify(response_data))
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == '__main__':
+    app.run(reload=True, debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,76 +1,122 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 
 import './App.css';
 
 function App() {
-  const [imageSrc, setImageSrc] = useState('');
-  const [cropper, setCropper] = useState();
-  const [cropData, setCropData] = useState();
-  const [fileMetadata, setFileMetadata] = useState();
+	const [imageSrc, setImageSrc] = useState('');
+	const [cropper, setCropper] = useState();
+	// const [cropData, setCropData] = useState();
+	// const [fileMetadata, setFileMetadata] = useState();
 
-  const [uploading, setUploading] = useState('false');
+	const [resultImages, setResultImages] = useState([]);
 
-  const onSelectFile = (event) => {
-    if (event.target.files && event.target.files.length > 0) {
-      setFileMetadata(event.target.files[0]);
+	const [uploading, setUploading] = useState(false);
 
-      const reader = new FileReader();
-      reader.addEventListener('load', () =>
-        setImageSrc(reader.result?.toString() || '')
-      );
-      reader.readAsDataURL(event.target.files[0]);
-    }
-  };
+	const onSelectFile = (event) => {
+		if (event.target.files && event.target.files.length > 0) {
+			// setFileMetadata(event.target.files[0]);
 
-  const submitQueryImage = async () => {
-    if (typeof cropper !== 'undefined') {
-      setCropData(cropper.getCroppedCanvas().toDataURL());
-    }
-  };
+			const reader = new FileReader();
+			reader.addEventListener('load', () =>
+				setImageSrc(reader.result?.toString() || '')
+			);
+			reader.readAsDataURL(event.target.files[0]);
+		}
+	};
 
-  return (
-    <div>
-      <input
-        type="file"
-        accept="image/png,image/jpeg"
-        onChange={onSelectFile}
-        disable={uploading}
-      />
-      {imageSrc && (
-        <div>
-          <Cropper
-            style={{ height: '50%', width: '50%' }}
-            minCropBoxHeight={10}
-            minCropBoxWidth={10}
-            preview=".img-preview"
-            viewMode={2}
-            src={imageSrc}
-            checkOrientation={false}
-            onInitialized={(instance) => {
-              setCropper(instance);
-            }}
-          />
+	const submitQueryImage = (event) => {
+		event.preventDefault();
+		if (typeof cropper !== 'undefined') {
+			const image = cropper.getCroppedCanvas().toDataURL();
 
-          <div className="box" style={{ width: '50%' }}>
-            <h1>Preview</h1>
-            <div
-              className="img-preview"
-              style={{ width: '100%', height: '200px' }}
-            />
-          </div>
+			const formData = new FormData();
+			formData.append('image', image);
+			formData.append('value', 2);
 
-          <div>
-            <button onClick={submitQueryImage} disable={uploading}>
-              Submit
-            </button>
-          </div>
-          <img src={cropData} alt='Result' />
-        </div>
-      )}
-    </div>
-  );
-}
+			setUploading(true);
+			axios
+				.post('http://localhost:5000/api/test2', formData)
+				.then((res) => {
+					setResultImages(res.data);
+					setUploading(false);
+				})
+				.catch((err) => {
+					setUploading(false);
+				});
+		};
+	};
+
+	console.log('fuck' + resultImages[0]?.image);
+	console.log('fuck1' + cropper?.getCroppedCanvas()?.toDataURL());
+
+	return (
+		<div>
+			<input
+				type="file"
+				accept="image/png,image/jpeg"
+				onChange={onSelectFile}
+				disable={uploading.toString()}
+			/>
+			{imageSrc && (
+				<div>
+					<Cropper
+						style={{ height: '50%', width: '50%' }}
+						minCropBoxHeight={10}
+						minCropBoxWidth={10}
+						preview=".img-preview"
+						viewMode={2}
+						src={imageSrc}
+						checkOrientation={false}
+						onInitialized={(instance) => {
+							setCropper(instance);
+						}}
+					/>
+
+					<div className="box" style={{ width: '50%' }}>
+						<h1>Preview</h1>
+						<div
+							className="img-preview"
+							style={{ width: '100%', height: '200px' }}
+						/>
+					</div>
+
+					<div>
+						<button
+							onClick={submitQueryImage}
+							disable={uploading.toString()}
+						>
+							Submit
+						</button>
+					</div>
+					{/* <img src={cropData} alt='Result' /> */}
+
+					<div className='result-container'>
+						{
+							!uploading ?
+								resultImages.map((result, index) => {
+									// console.log(result.image);
+									return (
+										<img
+											className='result-image'
+											key={index}
+											src={result.image}
+											alt={`Result ${index}`}
+										/>
+									);
+								})
+								: (
+									<div>Loading</div>
+								)
+						}
+
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default App;
